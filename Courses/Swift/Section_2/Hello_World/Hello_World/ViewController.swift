9984f5c1c890568ccda3a3bc0c5f8e151987aa3e
Alexandre Henrique <alexhenrii13@gmail.com>
2018-02-27 18:44:01 -0300

    Finished lesson 16, hello world with functional buttons

--- Courses/Swift/Section_2/Hello_World/Hello_World/ViewController.swift
+++ Courses/Swift/Section_2/Hello_World/Hello_World/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var profileLabel: UILabel!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        
+        profileLabel.text = nameTextField.text
+        
+        nameLabel.isHidden          = true
+        emailLabel.isHidden         = true
+        passwordLabel.isHidden      = true
+        emailTextField.isHidden     = true
+        passwordTextField.isHidden  = true
+        nameTextField.isHidden      = true
+        submitButton.isHidden       = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
