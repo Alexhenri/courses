c407c7c65ccfd1a377e6e5728bf2d6496d09ea34
Alexandre Henrique <alexhenrii13@gmail.com>
2018-02-27 20:37:22 -0300

    Finished section 2, Created cat years app

---
+++ Courses/Swift/Section_2/Cat Years/Cat Years/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Cat Years
+//
+//  Created by Alexandre Henrique Silva on 27/02/18.
+//  Copyright © 2018 Alexhenri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var ageResultLabel: UILabel!
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        resultLabel.isHidden = false
+        ageResultLabel.text = String(Int(ageTextField.text!)! * 7)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultLabel.isHidden = true
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
