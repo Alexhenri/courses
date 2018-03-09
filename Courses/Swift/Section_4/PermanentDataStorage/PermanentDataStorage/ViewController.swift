24dad03958cf8e1ce58d1b838e0a9c8382f4653b
Alexandre Henrique <alexhenrii13@gmail.com>
2018-03-09 17:59:35 -0300

    Learned how to do multiple view controllers, control the keyboard and storage permanent data

--- Courses/Swift/Section_4/PermanentDataStorage/PermanentDataStorage/ViewController.swift
+++ Courses/Swift/Section_4/PermanentDataStorage/PermanentDataStorage/ViewController.swift
@@ -10,16 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        /* Justing learnig UserDefaults
+        UserDefaults.standard.set("Alex", forKey: "name")
+
+        let nameObject = UserDefaults.standard.object(forKey: "name")
+        
+        //checking if has value AND is a string type
+        if let name = nameObject as? String {
+            print(name)
+        }
+        
+        let arrayTest = [1,2,3,4]
+        
+        UserDefaults.standard.set(arrayTest, forKey: "array")
+        
+        let arrayObject = UserDefaults.standard.object(forKey: "array")
+        
+        if let array = arrayObject as? NSArray {
+                print(array)
+        }
+        */
+        
+        let phoneNumberObject = UserDefaults.standard.object(forKey: "phone")
+        
+        if let phoneNumber = phoneNumberObject as? String {
+            phoneNumberTextField.text = phoneNumber
+            phoneNumberTextField.isEnabled = false
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func editButtonTapped(_ sender: Any) {
+        phoneNumberTextField.isEnabled = true
     }
-
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        
+        //using guard just to you give me a break .i.
+        
+        guard let phoneNumber = phoneNumberTextField.text else {
+            return
+        }
+        
+        UserDefaults.standard.set(phoneNumber, forKey:"phone")
+        phoneNumberTextField.isEnabled = false
+    }
 
 }
 
